386dd78febef6015564d9c6a17b87476488f2f78
Georg Niggli <jornig@outlook.com>
2024-04-05 20:04:50 +0200

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use chrono::{Datelike, Local, Timelike};
+use system_shutdown::shutdown;
+use wifiscanner;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        if is_between_friday_sunday_weekend() {
+            println!("{:?}", wifiscanner::scan());
+
+            let interfaces = wifiscanner::scan().unwrap();
+
+            for interface in interfaces {
+                if interface.ssid == "kue_ssid" {
+                    println!("Found the network! {}", interface.ssid);
+                    // shutdown the computer
+                    match shutdown() {
+                        Ok(_) => println!("Shutting down, bye!"),
+                        Err(error) => eprintln!("Failed to shut down: {}", error),
+                    }
+                }
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_secs(600));
+    }
+}
+
+fn is_between_friday_sunday_weekend() -> bool {
+    let now = Local::now();
+    let day_of_week = now.weekday();
+    let hour = now.hour();
+    if day_of_week == chrono::Weekday::Fri && (hour > 18 || (hour == 18)) {
+        return true;
+    }
+
+    if day_of_week == chrono::Weekday::Sat || day_of_week == chrono::Weekday::Sun {
+        return true;
+    }
+
+    if day_of_week == chrono::Weekday::Sun && (hour < 18 || (hour == 18)) {
+        return true;
+    }
+
+    false
 }
